0da928261c34e000218c2b2f1af03e633cd68461
MGSimard <mgsimard.dev@gmail.com>
2025-03-06 17:02:58 -0500

    Start narrowing down core skeleton, also I don't want a footer

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -9,24 +9,18 @@ export const Route = createFileRoute("/")({
 
 function PageIndex() {
   return (
-    <>
-      <Header>
-        <h1>Zodiak</h1>
-      </Header>
-      <main>
-        <Section>
-          <h2>Satoshi Font</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam at nibh sit amet dolor semper tincidunt a
-            eget diam. Curabitur sapien ligula, sodales quis arcu ac, accumsan tristique ex. Morbi id ante sit amet mi
-            auctor consequat. Aliquam imperdiet congue elit et scelerisque. Cras commodo fermentum nulla eget tempor.
-            Vestibulum a sollicitudin metus, id semper sapien. Aliquam vitae finibus nulla. Cras id ex at enim posuere
-            ultrices. Sed ut semper dolor. Donec tellus ligula, pharetra quis ex ut, gravida auctor lorem. Donec vitae
-            vulputate ligula. Vivamus nec metus risus. Morbi nulla mauris, tempor vel nibh gravida, ultrices posuere
-            arcu.
-          </p>
-        </Section>
-      </main>
-    </>
+    <main id="hero">
+      <Section>
+        <h2>Satoshi Font</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam at nibh sit amet dolor semper tincidunt a eget
+          diam. Curabitur sapien ligula, sodales quis arcu ac, accumsan tristique ex. Morbi id ante sit amet mi auctor
+          consequat. Aliquam imperdiet congue elit et scelerisque. Cras commodo fermentum nulla eget tempor. Vestibulum
+          a sollicitudin metus, id semper sapien. Aliquam vitae finibus nulla. Cras id ex at enim posuere ultrices. Sed
+          ut semper dolor. Donec tellus ligula, pharetra quis ex ut, gravida auctor lorem. Donec vitae vulputate ligula.
+          Vivamus nec metus risus. Morbi nulla mauris, tempor vel nibh gravida, ultrices posuere arcu.
+        </p>
+      </Section>
+    </main>
   );
 }
