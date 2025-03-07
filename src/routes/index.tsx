daf63e571f6a18d5d27582878c3566bfbf28142d
MGSimard <mgsimard.dev@gmail.com>
2025-03-07 10:49:58 -0500

    Stuff

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,9 +8,18 @@ function PageIndex() {
   return (
     <main>
       <section>
-        <h2>Void</h2>
-        <p>Text</p>
+        <Pre />
       </section>
     </main>
   );
 }
+
+const Pre = () => {
+  return (
+    <pre aria-label="VOID">{`██    ██  ██████  ██ ██████
+██    ██ ██    ██ ██ ██   ██
+██    ██ ██    ██ ██ ██   ██
+ ██  ██  ██    ██ ██ ██   ██
+  ████    ██████  ██ ██████`}</pre>
+  );
+};
