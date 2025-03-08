c82578e55c49c0d28d0f0555d654a2278d26a5ec
MGSimard <mgsimard.dev@gmail.com>
2025-03-08 15:59:44 -0500

    Put in sonner for some testing and code studying

--- src/_components/Toast.tsx
+++ src/_components/Toast.tsx
@@ -1,22 +1,3 @@
-// NOTES
-/**
- * All lines should always be 33 characters long.
- * First line is: .::::::::::: [ALERT] :::::::::::.
- * Padding line:  |                               |
- * Content line:  | One space padding left, fill  |
- * Content line:  | right right to match 33       |
- * Content line:  | length w/ 1 space padding     | (consider hyphen breaks)
- * Content line:  | minimum                       |
- * Padding line:  |                               |
- * Last line is:  '-------------------------------'
- *
- * Content lines should always have one space of padding on left
- * Content lines should always have at least one space of padding on right
- * Linebreak if text line is greater than 29 characters
- * Always fill remaining space with spacebars to finish 33 length
- * Hyphen break words if pre/post lines would each have at least 3 characters
- */
-
 import { wrapText33 } from "@/_utils/helpers";
 
 type TypeEnum = "info" | "warning" | "success" | "error" | "loading";
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,7 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { ThemeToggle } from "@/_components/ThemeToggle";
 import { Header } from "@/_components/Header";
+import { Toaster } from "sonner";
 
 export const Route = createRootRoute({
   component: () => (
@@ -9,6 +10,7 @@ export const Route = createRootRoute({
       <Outlet />
       <ThemeToggle />
       <div id="screen-glow"></div>
+      <Toaster />
     </>
   ),
 });
