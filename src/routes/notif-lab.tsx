e24837bf6a08fff3534b2bf3e65a3ce9e3b20f62
MGSimard <mgsimard.dev@gmail.com>
2025-03-08 17:38:46 -0500

    This is such a horrible idea for performance

--- src/routes/notif-lab.tsx
+++ src/routes/notif-lab.tsx
@@ -14,11 +14,11 @@ function PageNotifLab() {
         <button type="button" onClick={() => toast("Test")}>
           Test Regular Sonner
         </button>
-        <Notification message="This is a message test" />
-        <Notification message="This is a longer message test" />
         <Notification message="This is a much longer message test, this is a much longer message test, this is a much longer message test" />
         <Notification message="This is a looooooooooooooooooooooooooooooooooooooooooooooooooooooooong message test" />
         <Notification message="This is a loooooooooooooooooooooooong word message test, there should never be any overflow and the only times where a word should be hyphenated is if there were to be at least 3 characters in the before line, and three characters in the after line (not counting the hyphen) " />
+        <Notification message="This is a message test" />
+        <Notification message="This is a longer message test" />
       </section>
     </main>
   );
