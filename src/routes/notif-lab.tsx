93d8e9a8b225ec5f98e27d27d72392c4b950de7d
MGSimard <mgsimard.dev@gmail.com>
2025-03-08 17:56:48 -0500

    a

--- src/routes/notif-lab.tsx
+++ src/routes/notif-lab.tsx
@@ -14,13 +14,15 @@ function PageNotifLab() {
         <button type="button" onClick={() => toast("Test")}>
           Test Regular Sonner
         </button>
-        <button type="button" onClick={() => toast.custom(() => <Notification message="TEST" />)}>
+        <button
+          type="button"
+          onClick={() => toast.custom(() => <Notification message="A generic feedback message." />)}>
           Test Custom Sonner
         </button>
         <h2>Notification component</h2>
         <Notification message="This is a much longer message test, this is a much longer message test, this is a much longer message test" />
         <Notification message="This is a looooooooooooooooooooooooooooooooooooooooooooooooooooooooong message test" />
-        <Notification message="This is a loooooooooooooooooooooooong word message test, there should never be any overflow and the only times where a word should be hyphenated is if there were to be at least 3 characters in the before line, and three characters in the after line (not counting the hyphen) " />
+        <Notification message="This is a loooooooooooooooooooooooong word message test, there should never be any overflow and the only times where a word should be hyphenated is if there were to be at least 3 characters in the before line, and three characters in the after line (not counting the hyphen)" />
         <Notification message="This is a message test" />
         <Notification message="This is a longer message test" />
       </section>
