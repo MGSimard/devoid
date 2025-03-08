7ee94e77a37c6fff1249f19657317ed87d5e22de
MGSimard <mgsimard.dev@gmail.com>
2025-03-08 15:25:37 -0500

    minor typo mistake (biden blast)

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -33,7 +33,7 @@ function PageIndex() {
         <Toast message="This is a longer message test" />
         <Toast message="This is a much longer message test, this is a much longer message test, this is a much longer message test" />
         <Toast message="This is a looooooooooooooooooooooooooooooooooooooooooooooooooooooooong message test" />
-        <Toast message="This is a loooooooooooooooooooooooong word message test, there should never be any overflow and the only times where a word should by hyphenated is if there were to be at least 3 characters in the before line, and three characters in the after line (not counting the hyphen) " />
+        <Toast message="This is a loooooooooooooooooooooooong word message test, there should never be any overflow and the only times where a word should be hyphenated is if there were to be at least 3 characters in the before line, and three characters in the after line (not counting the hyphen) " />
       </section>
     </main>
   );
