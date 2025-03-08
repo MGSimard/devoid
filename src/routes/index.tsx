9cb77ee90fdc1cbac3c36988d286a30649a1712b
MGSimard <mgsimard.dev@gmail.com>
2025-03-08 08:12:59 -0500

    Maybe?

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -14,6 +14,11 @@ function PageIndex() {
         <h2>Tooltip planning</h2>
         <Tooltip />
       </section>
+      <section>
+        <h2>Notification planning</h2>
+        <p>Bit like sonner?</p>
+        <Notification />
+      </section>
     </main>
   );
 }
@@ -34,14 +39,25 @@ function Pre() {
 function Tooltip() {
   return (
     <pre>{`**********************************************************
-*                                                        *
 * Since we're monospace I could calc character amount    *
 * and then adjust the amount of asterixes for emulated   *
 * space in the tooltip. Also have to consider current    *
 * viewport width, if position would render outside       *
 * the viewport, current screen zoom relative to viewport *
 * width etc.                                             *
-*                                                        *
 **********************************************************`}</pre>
+  );
+}
+
+function Notification() {
+  return (
+    <pre>{`.::::::::::: [Notification] :::::::::::.
+|                                      |
+| Would pop up similarly to Sonner     |
+| Again, adjust frame character length |
+| depending on inner chars with a max  |
+| width?                               |
+|                                      |
+'--------------------------------------'`}</pre>
   );
 }
