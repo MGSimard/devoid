66beb2d1b60ccf531d0fd26e94eb618c8830bf5b
MGSimard <mgsimard.dev@gmail.com>
2025-03-08 17:57:09 -0500

    Clean up notif section only use the page for now

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -20,14 +20,6 @@ function PageIndex() {
           Test Link
         </a>
       </section>
-      <section>
-        <h2>Notification component test</h2>
-        <Notification message="This is a message test" />
-        <Notification message="This is a longer message test" />
-        <Notification message="This is a much longer message test, this is a much longer message test, this is a much longer message test" />
-        <Notification message="This is a looooooooooooooooooooooooooooooooooooooooooooooooooooooooong message test" />
-        <Notification message="This is a loooooooooooooooooooooooong word message test, there should never be any overflow and the only times where a word should be hyphenated is if there were to be at least 3 characters in the before line, and three characters in the after line (not counting the hyphen) " />
-      </section>
     </main>
   );
 }
