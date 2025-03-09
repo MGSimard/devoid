f25505a8743cf52b70b5a17f44af6f4d0e3cff7e
MGSimard <mgsimard.dev@gmail.com>
2025-03-08 19:49:57 -0500

    Update tasks

--- README.md
+++ README.md
@@ -3,12 +3,10 @@
 # Task List
 
 - [ ] Make sonners more readable over content
-- [ ] See if there's a way to dump the helper function entirely for dynamic ASCII notifications, maybe there's a play somewhere with just html/css? Hard to imagine if we want to strictly stay within ASCII territory
+- [ ] Button styles
 - [ ] .link styles (still need discernability when not hovered/focus-visible)
 - [ ] Figure out if I can stop glow bleeds between lines for ASCII art while still having glow
 - [ ] ...
 - [ ] Fuck native title tooltip, come up with something else + keep aria-label
 - [ ] Per-character deciphering animation on tooltip?
-- [ ] What background color for tooltip? Same as base?
-- [ ] But if I use border it's not longer character-only based UI, so figure out a character-only frame to the tooltip box?
 - [x] Firefox mobile still an issue for position fixed objects that are either bottom-fixed or inset: 0; Scrolling down/zooming in which collapses the top browser bar creates a dead space at the bottom which is a bug on firefox mobile only - eat shit at this point. Tired of the moz developers. Fix this shit, fix gradients, implement view transitions, y'all are fucking embarrassing.
--- src/routes/echo.tsx
+++ src/routes/echo.tsx
@@ -10,19 +10,19 @@ function PageEcho() {
   return (
     <main>
       <section>
-        <h2>Notification component test</h2>
+        <h2>Echo Component Test</h2>
         <button type="button" onClick={() => toast("Test")}>
           Test Regular Sonner
         </button>
         <button type="button" onClick={() => toast.custom(() => <CustomToast message="A generic feedback message." />)}>
-          Test Custom Sonner
+          Test Echo
         </button>
         <button
           type="button"
           onClick={() => toast.custom(() => <CustomToast message="A generic feedback message." type="error" />)}>
-          Test Custom ERROR Sonner
+          Test Echo (error)
         </button>
-        <h2>Notification component</h2>
+        <h2>Echo component</h2>
         <CustomToast message="This is a much longer message test, this is a much longer message test, this is a much longer message test" />
         <CustomToast message="This is a looooooooooooooooooooooooooooooooooooooooooooooooooooooooong message test" />
         <CustomToast message="This is a loooooooooooooooooooooooong word message test, there should never be any overflow and the only times where a word should be hyphenated is if there were to be at least 3 characters in the before line, and three characters in the after line (not counting the hyphen)" />
